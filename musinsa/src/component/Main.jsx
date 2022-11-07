f3a35243e93134f15a5d7d8e268d909ae28952bf
JungKi Kim <73014464+omago123@users.noreply.github.com>
2022-11-07 18:40:50 +0900

    화면구성

---
+++ musinsa/src/component/Main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Main.css'
+
+function Main() {
+  return (
+    <>
+      <div>
+        <img src="banner.png"></img>        
+        <div className='top'>
+          <a>MUSINSA</a>
+        </div>
+      </div>
+      <div className='detail'>
+        <img id="clothimg" src="947088_3_500.jpg"></img>
+      </div>
+
+    </>
+  )
+}
+
+export default Main
